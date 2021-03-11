bfba76b0109287d50d7f92d04215a7bb3b9445ca
yaelchicco14@gmail.com <yaelchicco14@gmail.com>
2021-03-11 14:27:57 +0200

    initial commit

--- src/primitives/Point3D.java
+++ src/primitives/Point3D.java
@@ -1,17 +1,73 @@
 package primitives;
 
+import java.security.PublicKey;
+
 public class Point3D {
     final Coordinate _x;
     final Coordinate _y;
     final Coordinate _z;
+    public final static Point3D ZERO = new Point3D(0d, 0d, 0d);
 
+    /**
+     * primary constructor for 3D Point
+     *
+     * @param x value for x Coordinate
+     * @param y value for y Coordinate
+     * @param z value for z Coordinate
+     */
     public Point3D(Coordinate x, Coordinate y, Coordinate z) {
         _x = x;
         _y = y;
         _z = z;
     }
 
+    /**
+     * @param x value for creating x Coordinate
+     * @param y value for creating y Coordinate
+     * @param z value for creating z Coordinate
+     */
+
+    public Point3D(double x, double y, double z) {
+        this(new Coordinate(x), new Coordinate(y), new Coordinate(z));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point3D point3D = (Point3D) o;
+        return _x.equals(point3D._x) && _y.equals(point3D._y) && _z.equals(point3D._z);
+    }
+
+    public Point3D add(Vector vector) {
+        double x=_x.coord+vector._head._x.coord;
+        double y=_y.coord+vector._head._y.coord;
+        double z=_z.coord+vector._head._z.coord;
+
+        return new Point3D(x,y,z);
+    }
+
     public Vector subtract(Point3D point) {
-        return null;
+        double x=_x.coord-point._x.coord;
+        double y=_y.coord-point._y.coord;
+        double z=_z.coord-point._z.coord;
+
+        return new Vector(x,y,z);
+    }
+
+    public double distanceSquared(Point3D other){
+        double xx=(other._x.coord- _x.coord)*(other._x.coord- _x.coord);
+        double yy=(other._y.coord- _y.coord)*(other._y.coord- _y.coord);
+        double zz=(other._z.coord- _z.coord)*(other._z.coord- _z.coord);
+
+        return (xx+yy+zz);
+    }
+
+    public double distance(Point3D other) {
+        return Math.sqrt(distanceSquared(other));
+    }
+        @Override
+    public String toString() {
+        return "Point3D(" + "" + _x + "," + _y + "," + _z + ')';
     }
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,6 +1,45 @@
 package primitives;
 
+import static primitives.Point3D.ZERO;
+import static primitives.Util.isZero;
+
 public class Vector {
+    Point3D _head;
+
+    /**
+     * primary constructor for Vector
+     *
+     * @param head
+     */
+    public Vector(Point3D head) {
+        if (ZERO.equals(head)) {
+            throw new IllegalArgumentException("head of vector cannot be point(0,0,0,)");
+        }
+
+        _head = new Point3D(head._x.coord,head._y.coord,head._z.coord);
+    }
+
+    public Vector(double x, double y, double z) {
+        this(new Point3D(x, y, z));
+    }
+
+    public Vector(Coordinate x, Coordinate y, Coordinate z) {
+        this(new Point3D(x, y, z));
+    }
+
+    public Point3D getHead() {
+        return _head;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vector vector = (Vector) o;
+        return _head.equals(vector._head);
+    }
+
     public Vector crossProduct(Vector vector) {
         return null;
     }
@@ -8,4 +47,52 @@ public class Vector {
     public double dotProduct(Vector vector) {
         return 0d;
     }
+
+    public Vector subtract(Vector other) {
+        if (other.equals(this)) {
+            throw new IllegalArgumentException("parameter vector cannot be equals to this vector");
+        }
+
+        return other._head.subtract(_head);
+    }
+
+    public double lengthSquared() {
+        double xx = (_head._x.coord * _head._x.coord);
+        double yy = (_head._y.coord * _head._y.coord);
+        double zz = (_head._z.coord * _head._z.coord);
+        return xx + yy + zz;
+    }
+
+    public double length() {
+        return Math.sqrt(lengthSquared());
+    }
+
+    public Vector normalize() {
+
+        double len = length();
+
+        if (isZero(len)) {
+            throw new ArithmeticException("length equals 0");
+        }
+
+        double x = _head._x.coord / len;
+        double y = _head._y.coord / len;
+        double z = _head._z.coord / len;
+
+        _head = new Point3D(x, y, z);
+
+        if (ZERO.equals(_head)) {
+            throw new IllegalArgumentException("point(0,0,0) vector");
+        }
+
+        return this;
+    }
+
+    public Vector normalized() {
+        Vector result=new Vector(_head);
+        result.normalize();
+        return result;
+    }
+
+
 }
