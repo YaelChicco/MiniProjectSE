7e5152f30a3f291edd1f5916ad48095107f1f379
yaelchicco14@gmail.com <yaelchicco14@gmail.com>
2021-03-14 19:49:34 +0200

    sphere tube

---
+++ src/geometries/Tube.java
@@ -0,0 +1,14 @@
+package geometries;
+
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
+
+public class Tube implements Geometry{
+private Ray axisRay;
+private double radius;
+    @Override
+    public Vector getNormal(Point3D point) {
+        return null;
+    }
+}
