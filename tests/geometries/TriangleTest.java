2ec30ef933f790630658c12d27dd79e4d39cdd20
avigayiltessler <avigayiltess@gmail.com>
2021-04-06 18:13:01 +0300

    finished get normal test of: polygon, plane, triangle

--- tests/geometries/TriangleTest.java
+++ tests/geometries/TriangleTest.java
@@ -1,16 +1,20 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point3D;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static primitives.Util.isZero;
 
 class TriangleTest {
 
     @Test
     void testGetNormal() {
-    }
+        Triangle tr=new Triangle(new Point3D(0, 0, 1), new Point3D(1, 0, 0), new Point3D(0, 1, 0));
+        double sqrt3 = Math.sqrt(1d / 3);
 
-    @Test
-    void testTestGetNormal() {
+        assertEquals( new Vector(sqrt3, sqrt3, sqrt3), tr.getNormal(new Point3D(1d / 3, 1d / 3, 1d / 3)),"Bad normal to triangle");
+        assertTrue( isZero(tr.getNormal(new Point3D(0, 0, 1)).lengthSquared()-1),"Bad normal to triangle");
     }
 }
