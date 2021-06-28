fdaa36d27d17ee359a589722f970c41dda5d37c6
yaelchicco14@gmail.com <yaelchicco14@gmail.com>
2021-06-28 18:08:57 +0300

    mini project

--- tests/FinalProject.java
+++ tests/FinalProject.java
@@ -1,6 +1,7 @@
 import elements.AmbientLight;
 import elements.Camera;
 import elements.DirectionalLight;
+import elements.PointLight;
 import geometries.Plane;
 import geometries.Polygon;
 import geometries.Sphere;
@@ -17,19 +18,21 @@ import scene.Scene;
 
 public class FinalProject {
     private static final Material mat = new Material().setKd(0.5).setKs(0.5).setShininess(30);
+    private static final Material transMat = new Material().setKd(0.2).setKs(0.2).setShininess(35).setkT(0.4);
+
     private Scene scene = new Scene("Test scene");
     private Camera camera = new Camera(new Point3D(1000, -600, 20), new Vector(-50, 30, -1), new Vector(-50, 30, 3400)) //
-            .setViewPlaneSize(200, 200).setDistance(1000);
+            .setViewPlaneSize(200, 200).setDistance(1000).setAperture(16,3).setFocalDistance(1166);
 
     @Test
     public void myPicture() {
-        scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.gray), 0.15)).setBackGround(new Color(0, 0, 51));
+        scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.gray), 0.15)).setBackGround(new Color(0, 0, 30));
         scene.geometries.add(
 
                 //sea
                 new Plane(new Point3D(0, 0, -50), new Vector(0, 0, 1))
-                        .setEmission(new Color(23, 23, 79))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(35).setkR(0.1)),
+                        .setEmission(new Color(0, 0, 26))
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(10).setkR(0.1)),
 
                 //island
                 new Sphere(70, new Point3D(0, 0, -70))
@@ -80,20 +83,76 @@ public class FinalProject {
                 new Polygon(new Point3D(-11.05, 11.05, 62), new Point3D(-10.05, 10.05, 80), new Point3D(10.05, 10.05, 80), new Point3D(11.05, 11.05, 62))
                         .setEmission(new Color(200, 0, 0)).setMaterial(mat),
 
+                //door
+                new Polygon(new Point3D(15.06, -4, -10), new Point3D(14.06, -3, 8), new Point3D(14.06, 3, 8), new Point3D(15.06, 4, -10))
+                        .setEmission(new Color(77,38,0)),
+
+                //blue thing
+                new Polygon(new Point3D(12,-12,80),new Point3D(-12,-12,80),new Point3D(-12,12,80),new Point3D(12,12,80))
+                        .setEmission(new Color(102,102,153)).setMaterial(mat),
+                new Polygon(new Point3D(12,-12,85),new Point3D(-12,-12,85),new Point3D(-12,12,85),new Point3D(12,12,85))
+                        .setEmission(new Color(102,102,153)).setMaterial(mat),
+                new Polygon(new Point3D(12,-12,80),new Point3D(12,-12,85),new Point3D(12,12,85),new Point3D(12,12,80))
+                        .setEmission(new Color(102,102,153)).setMaterial(mat),
+                new Polygon(new Point3D(-12,-12,80),new Point3D(-12,-12,85),new Point3D(12,-12,85),new Point3D(12,-12,80))
+                        .setEmission(new Color(102,102,153)).setMaterial(mat),
+                new Polygon(new Point3D(-12,-12,80),new Point3D(-12,-12,85),new Point3D(-12,12,85),new Point3D(-12,12,80))
+                        .setEmission(new Color(102,102,153)).setMaterial(mat),
+                new Polygon(new Point3D(-12,12,80),new Point3D(-12,12,85),new Point3D(-12,-12,85),new Point3D(-12,-12,80))
+                        .setEmission(new Color(102,102,153)).setMaterial(mat),
+
+                //windows
+                 new Polygon(new Point3D(10,-10,85),new Point3D(10,-10,95),new Point3D(10,10,95),new Point3D(10,10,85))
+                        .setEmission(new Color(255,255,0)).setMaterial(transMat),
+                new Polygon(new Point3D(-10,-10,85),new Point3D(-10,-10,95),new Point3D(10,-10,95),new Point3D(10,-10,85))
+                        .setEmission(new Color(255,255,0)).setMaterial(transMat),
+                new Polygon(new Point3D(-10,-10,85),new Point3D(-10,-10,95),new Point3D(-10,10,95),new Point3D(-10,10,85))
+                        .setEmission(new Color(255,255,0)).setMaterial(transMat),
+                new Polygon(new Point3D(-10,10,85),new Point3D(-10,10,95),new Point3D(-10,-10,95),new Point3D(-10,-10,85))
+                        .setEmission(new Color(255,255,0)).setMaterial(transMat),
+
                 //roof
-                new Triangle(new Point3D(10, -10, 80), new Point3D(10, 10, 80), new Point3D(0, 0, 100))
-                        .setEmission(new Color(200,200,200)).setMaterial(mat),
-                new Triangle(new Point3D(-10, -10, 80), new Point3D(10, -10, 80), new Point3D(0, 0, 100))
-                        .setEmission(new Color(200,200,200)).setMaterial(mat),
-                new Triangle(new Point3D(-10, -10, 80), new Point3D(-10, 10, 80), new Point3D(0, 0, 100))
-                        .setEmission(new Color(200,200,200)).setMaterial(mat),
-                new Triangle(new Point3D(-10, 10, 80), new Point3D(10, 10, 80), new Point3D(0, 0, 100))
-                        .setEmission(new Color(200,200,200)).setMaterial(mat)
+                new Polygon(new Point3D(12,-12,95),new Point3D(-12,-12,95),new Point3D(-12,12,95),new Point3D(12,12,95))
+                        .setEmission(new Color(102,102,153)).setMaterial(mat),
+                new Triangle(new Point3D(12, -12, 95), new Point3D(12, 12, 95), new Point3D(0, 0, 105))
+                        .setEmission(new Color(102,102,153)).setMaterial(mat),
+                new Triangle(new Point3D(-12, -12, 95), new Point3D(12, -12, 95), new Point3D(0, 0, 105))
+                        .setEmission(new Color(102,102,153)).setMaterial(mat),
+                new Triangle(new Point3D(-12, -12, 95), new Point3D(-12, 12, 95), new Point3D(0, 0, 105))
+                        .setEmission(new Color(102,102,153)).setMaterial(mat),
+                new Triangle(new Point3D(-12, 12, 95), new Point3D(12, 12, 95), new Point3D(0, 0, 105))
+                        .setEmission(new Color(2102,102,153)).setMaterial(mat),
+
+                //moon
+                new Sphere(18, new Point3D(-150,-30,100)).setEmission(new Color(110,110,110)).setMaterial(transMat),
+
+                //stars
+                new Sphere(1, new Point3D(-150,50,80)).setEmission(new Color(110,110,110)).setMaterial(mat),
+                new Sphere(2.5, new Point3D(-150,135,95)).setEmission(new Color(110,110,110)).setMaterial(mat),
+                new Sphere(1.5, new Point3D(-150,180,125)).setEmission(new Color(110,110,110)).setMaterial(mat),
+                new Sphere(1, new Point3D(-150,220,60)).setEmission(new Color(110,110,110)).setMaterial(mat),
+                new Sphere(1.8, new Point3D(-150,150,40)).setEmission(new Color(110,110,110)).setMaterial(mat),
+                new Sphere(1, new Point3D(-150,240,102)).setEmission(new Color(110,110,110)).setMaterial(mat),
+                new Sphere(2, new Point3D(-150,23,100)).setEmission(new Color(110,110,110)).setMaterial(mat),
+                new Sphere(2, new Point3D(-150,-3,50)).setEmission(new Color(110,110,110)).setMaterial(mat),
+                new Sphere(2, new Point3D(-150,-50,70)).setEmission(new Color(110,110,110)).setMaterial(mat),
+                new Sphere(1, new Point3D(-150,-35,30)).setEmission(new Color(110,110,110)).setMaterial(mat)
+
+
         );
-        scene.lights.add(new DirectionalLight(new Color(80, 80, 80), new Vector(-50, -30, -100)));
+
+        scene.lights.add(new DirectionalLight(new Color(80, 80, 80), new Vector(150, 30, -100)));
+        scene.lights.add(new PointLight(new Color(200, 200, 200),new Point3D(9.9,0,90)));
+        scene.lights.add(new PointLight(new Color(200, 200, 200),new Point3D(-9.9,0,90)));
+        scene.lights.add(new PointLight(new Color(200, 200, 200),new Point3D(0,9.9,90)));
+        scene.lights.add(new PointLight(new Color(200, 200, 200),new Point3D(0,-9.9,90)));
+
+
         Render render = new Render() //
                 .setImageWriter(new ImageWriter("LightHouse", 600, 600)) //
                 .setCamera(camera) //
+                .setMultithreading(3)
+                .setMultyRay(false)
                 .setRayTracer(new BasicRayTracer(scene));
         render.renderImage();
         render.writeToImage();
